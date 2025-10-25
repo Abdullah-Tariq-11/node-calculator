007a0f9fb48e757a92f8cb83cb29f971398aaa1f
AbdullahTariq74 <abdtariq3274@gmail.com>
2025-10-25 10:19:30 +0500

    Added Modulus function && version to 1.1.0

--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,7 @@
 
 // calculator.test.js
-const { add, subtract, multiply, divide } = require('./calculator');
+// calculator.test.js
+const { add, subtract, multiply, divide, modulus } = require('./calculator');
 
 describe('Calculator', () => {
   test('add should return sum', () => {
@@ -10,17 +11,31 @@ describe('Calculator', () => {
 
   test('subtract should return difference', () => {
     expect(subtract(5, 3)).toBe(2);
+    expect(subtract(3, 5)).toBe(-2);
   });
 
   test('multiply should return product', () => {
     expect(multiply(4, 5)).toBe(20);
+    expect(multiply(-2, 3)).toBe(-6);
   });
 
   test('divide should return quotient', () => {
     expect(divide(10, 2)).toBe(5);
+    expect(divide(9, 3)).toBe(3);
   });
 
   test('divide should throw an error when dividing by zero', () => {
-    expect(() => divide(5, 0)).toThrow('Divide by zero');
+    expect(() => divide(5, 0)).toThrow('Cannot divide by zero');
+  });
+
+  // For Modulus
+  test('modulus should return remainder', () => {
+    expect(modulus(10, 3)).toBe(1);
+    expect(modulus(25, 5)).toBe(0);
+  });
+
+  test('modulus should throw an error when modulus by zero', () => {
+    expect(() => modulus(7, 0)).toThrow('Cannot take modulus by zero');
   });
 });
+
